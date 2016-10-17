0cfe5d5da7b020bdb215d477b03c5d37aac295ea
Fredrik Westermark <fredrik.westermark@trioptima.com>
2016-10-17 22:58:17 +0200

    Add shell script to run tests for several drivers and all session backends

---
+++ run_tests.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+session_engines=(
+"django.contrib.sessions.backends.db"
+"django.contrib.sessions.backends.file"
+"django.contrib.sessions.backends.cache"
+"django.contrib.sessions.backends.cached_db"
+"django.contrib.sessions.backends.signed_cookies"
+)
+
+for session in "${session_engines[@]}"
+do
+    export SESSION_ENGINE=$session
+    tox -- --driver=Chrome
+    tox -- --driver=Firefox
+    tox -- --driver=PhantomJS
+done
