e32d9bad08a108ff7ecb905ccb33095cf82d0e0d
Fredrik Westermark <fredrik.westermark@trioptima.com>
2018-10-20 21:28:15 +0200

    Use is_authenticated as property. Function call is deprecated

--- test_/views.py
+++ test_/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 def login_required(request):
     if 'user' in dir(request):
-        if request.user.is_authenticated():
+        if request.user.is_authenticated:
             return HttpResponse('success')
     return HttpResponse('fail')
 
